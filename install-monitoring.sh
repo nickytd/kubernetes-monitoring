793fae278efd747728fb05ab8b36bcbd212c54fd
Niki Dokovski <nikolai.dokovski@sap.com>
2021-02-15 14:11:06 +0100

    update setup

--- install-monitoring.sh
+++ install-monitoring.sh
@@ -5,30 +5,18 @@ set -eo pipefail
 dir=$(dirname $0)
 
 echo "setting up monitoring stack"
+echo "options: --with-blackbox-exporter --with-karma --with-lb --with-ingress-nginx"
 
 kubectl create namespace monitoring \
   --dry-run=client -o yaml | kubectl apply -f -
 
-kubectl apply -f $dir/monitoring/blackbox-targets.yaml -n monitoring \
-   --dry-run=client -o yaml | kubectl apply -f -
-
 helm upgrade monitoring -n monitoring \
    -f $dir/monitoring/kube-prometheus-values.yaml prometheus-community/kube-prometheus-stack \
    --install --wait --timeout 15m
-
-helm upgrade blackbox-exporter -n monitoring  \
-   -f $dir/monitoring/blackbox-exporter-values.yaml prometheus-community/prometheus-blackbox-exporter \
-   --install --wait --timeout 15m
-
-helm upgrade karma -n monitoring \
-    -f $dir/monitoring/karma-values.yaml stable/karma \
-    --install --wait --timeout 15m
     
 kubectl apply -f $dir/monitoring/system-rules.yaml -n monitoring \
    --dry-run=client -o yaml | kubectl apply -f -    
-
-kubectl apply -f $dir/monitoring/blackbox-rules.yaml -n monitoring \
-   --dry-run=client -o yaml | kubectl apply -f -   
+  
 
 for dashboard in $dir/monitoring/grafana-dashboards/* ; do
 echo "processing "$(basename $dashboard)""
@@ -39,6 +27,28 @@ done
 
 for var in "$@"
 do
+
+    if [[ "$var" = "--with-karma" ]]; then
+      helm upgrade karma -n monitoring \
+        -f $dir/monitoring/karma-values.yaml stable/karma \
+        --install --wait --timeout 15m
+
+    fi  
+
+    if [[ "$var" = "--with-blackbox-exporter" ]]; then 
+
+      echo "creating blackbox-exporter"
+      kubectl apply -f $dir/monitoring/blackbox-targets.yaml -n monitoring \
+        --dry-run=client -o yaml | kubectl apply -f -
+      
+      helm upgrade blackbox-exporter -n monitoring  \
+        -f $dir/monitoring/blackbox-exporter-values.yaml prometheus-community/prometheus-blackbox-exporter \
+        --install --wait --timeout 15m
+
+      kubectl apply -f $dir/monitoring/blackbox-rules.yaml -n monitoring \
+        --dry-run=client -o yaml | kubectl apply -f -   
+    fi
+
     if [[ "$var" = "--with-lb" ]]; then
       echo "creating kibana LoadBalancer"
 
